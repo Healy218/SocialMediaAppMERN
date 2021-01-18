3c7960ad5b332961ab6b7a81782e0deb0ea6c616
healy218 <mrhealy115@gmail.com>
2021-01-18 16:19:43 -0600

    Initial set up

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+	'mongodb+srv://mrhealy115:rASkZU1bsy0ek3iR@cluster0.ahow6.mongodb.net/Services?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+	.connect(CONNECTION_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
+	.catch((error) => console.log(error.message));
+
+//mongoose.set('useFindandModify', false);
