913c2e77b3977b394e35550af75a23e462043f62
healy218 <mrhealy115@gmail.com>
2021-01-19 17:08:10 -0600

    Added post functionality

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
+import { createPost } from '../../actions/posts';
 
 import styles from './styles';
 
@@ -13,8 +15,13 @@ const Form = () => {
 		selectedFile: '',
 	});
 	const classes = styles();
+	const dispatch = useDispatch();
 
-	const handleSubmit = () => {};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		dispatch(createPost(postData));
+	};
 
 	const clear = () => {};
 	return (
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -3,18 +3,28 @@ import Post from './Post/Post.js';
 import { useSelector } from 'react-redux';
 
 import styles from './styles';
+import { CircularProgress, Grid } from '@material-ui/core';
 
 const Posts = () => {
 	const posts = useSelector((state) => state.posts);
 	const classes = styles();
 
 	console.log(posts);
-	return (
-		<>
-			<h1>POSTS</h1>
-			<Post />
-			<Post />
-		</>
+	return !posts.length ? (
+		<CircularProgress />
+	) : (
+		<Grid
+			className={classes.container}
+			container
+			alignItems='stretch'
+			spacing={3}
+		>
+			{posts.map((post) => (
+				<Grid key={post._id} item xs={12} sm={6}>
+					<Post post={post} />
+				</Grid>
+			))}
+		</Grid>
 	);
 };
 
